6158d6f67b6422115f84da45ace3ad6ca0b14607
Carlos Nava <machinery.e@gmail.com>
2022-11-28 16:23:49 -0400

    Crear interface adaptadora

--- src/bases/04-injection.ts
+++ src/bases/04-injection.ts
@@ -1,30 +1,41 @@
-import { PokeApiAdapter } from '../core/api/poke-api.adapter';
-import { Move } from '../models';
+import { HttpAdapter, PokeApiAdapter } from '../core/api/poke-api.adapter';
+import { Move, PokeAPIResponse } from '../models';
 
 
 export class PokemonInjection {
 
     get imageUrl(): string {
-        return this.pokeapi.getImage(this.id);
+        return this.pokeapi.getPokeImage(this.id);
     }
-  
+    /**
+     * Constructor de la clase
+     * @param id del pokemon
+     * @param name nombre dle pokemon
+     * @param pokeapi Adaptador
+     */
     constructor(
         public readonly id: number, 
         public name: string,
-        private pokeapi: PokeApiAdapter
+        private pokeapi: HttpAdapter
 
     ) {}
-
+    /**
+     * Gritar
+     */
     scream() {
         return `${ this.name.toUpperCase() }!!!`
     }
-
+    /**
+     * Hablar
+     */
     speak() {
         return `${ this.name }, ${ this.name }`
     }
-
+    /**
+     * Obtener movimientos
+     */
     async getMoves(): Promise<Move[]> {
-        const data = await this.pokeapi.getPokemon(this.name);
+        const data = await this.pokeapi.getPokemon<PokeAPIResponse>(this.name);
         console.log(this.name, data.moves );
         return data.moves;
     }
--- src/core/api/poke-api.adapter.ts
+++ src/core/api/poke-api.adapter.ts
@@ -1,10 +1,57 @@
 import axios from "axios";
-import { PokeAPIResponse } from "../../models";
 
 const POKEAPI = 'https://pokeapi.co/api/v2/pokemon/';
 const POKEAPI_IMG = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/';
 
-export class PokeApiAdapter {
+/**
+ * construir el id
+ * @param id 
+ */
+const ConstructId = (id: number) => {
+    const newId = id.toString();
+    if(newId.length === 1 ){
+        return '00'+newId;
+    }
+    if(newId.length === 2 ){
+        return '0'+newId;
+    }
+    if(newId.length === 3 ){
+        return newId;
+    }
+    return newId;
+}
+const GetImage = (id: number): string => {
+    const newId = ConstructId(id);
+    return POKEAPI_IMG + newId + '.png';
+}
+/**
+ * Interface para adaptar metodos de consulta
+ */
+export interface HttpAdapter {
+    getPokemon<T>(name: string): Promise<T>;
+    getPokeImage(id: number): string;
+}
+/**
+ * Adaptador de vanilla
+ */
+export class PokeApiFetchAdapter implements HttpAdapter{
+    async getPokemon<T>(name: string): Promise<T> {
+        const resp = await fetch(POKEAPI + name.toLocaleLowerCase());
+        const data: T = await resp.json();
+        return data;
+    }
+    /**
+     * Obtiene la imagen de un pokemon
+     * @param id 
+     */
+    getPokeImage(id: number): string {
+        return GetImage(id);
+    }
+}
+/**
+ * Adaptador de AXIOS
+ */
+export class PokeApiAdapter implements HttpAdapter{
     constructor(
         private readonly axiosService = axios
     ) {}
@@ -12,43 +59,18 @@ export class PokeApiAdapter {
      * Obtener datos de un pokemon
      * @param name Nombre del pokemon
      */
-    async getPokemon(name: string): Promise<PokeAPIResponse> {
-        const { data } = await this.axiosService.get<PokeAPIResponse>( POKEAPI + name.toLocaleLowerCase() );
+    async getPokemon<T>(name: string): Promise<T> {
+        const { data } = await this.axiosService.get<T>( POKEAPI + name.toLocaleLowerCase() );
         return data;
     }
     /**
      * Obtiene la imagen de un pokemon
      * @param id 
      */
-    getPokeImage(id: string) {
-        return POKEAPI_IMG + id + '.png';
-    }
-    /**
-     * Obtiene la imagen de un pokemon
-     * @param id 
-     */
-    getImage(id: number) {
-        const newId = this.constructId(id);
-        return POKEAPI_IMG + newId + '.png';
+    getPokeImage(id: number): string {
+        return GetImage(id);
     }
     async post(url: string) { return;}
     async path(url: string) { return;}
     async delete(url: string) { return;}
-    /**
-     * construir el id
-     * @param id 
-     */
-    public constructId(id: number) {
-        const newId = id.toString();
-        if(newId.length === 1 ){
-            return '00'+newId;
-        }
-        if(newId.length === 2 ){
-            return '0'+newId;
-        }
-        if(newId.length === 3 ){
-            return newId;
-        }
-        return newId;
-    }
 }
